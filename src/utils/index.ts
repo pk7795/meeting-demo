162277ecb3f3ca21dad3ceb28fb9344ecd0f43e6
caohv <caohv.dev@gmail.com>
2023-07-21 15:51:23 +0700

    fix: generate short passcode

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,26 @@
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
-import { v4 } from 'uuid'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
 export const formatDate = (date: any) => (date ? dayjs(date).format('DD/MM/YYYY HH:mm:ss') : '')
 
-export const genPasscode = () => v4()
+export const genPasscode = () => {
+  const length = 10
+  const date = Date.now().toString()
+
+  let charset = ''
+  charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  charset += 'abcdefghijklmnopqrstuvwxyz'
+  charset += date
+
+  let password = ''
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length))
+  }
+  return password
+}
 
 export const formatDateChat = (date: Date) => (date ? dayjs(date).format('HH:mm') : '')
