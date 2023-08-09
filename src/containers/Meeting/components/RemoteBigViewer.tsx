a7c6d9dbc96421709199390008ff1ce35fd30111
Giang Minh <giangndm@ows.vn>
2023-08-09 18:20:47 +0700

    feat: added force high button in screen view

--- src/containers/Meeting/components/RemoteBigViewer.tsx
+++ src/containers/Meeting/components/RemoteBigViewer.tsx
@@ -1,9 +1,9 @@
 import { BigViewer } from '.'
 import { BlueseaSenders, BlueseaStreamPriority, MIN_AUDIO_LEVEL } from '../constants'
 import { Stream } from '../types'
+import { Switch } from 'antd'
 import { useAudioLevelMix } from 'bluesea-media-react-sdk'
-import classNames from 'classnames'
-import { FC, useMemo } from 'react'
+import { FC, useMemo, useState } from 'react'
 import { usePeerRemoteStreamActive } from '@/hooks'
 import { MeetingParticipant } from '@/types/types'
 
@@ -12,6 +12,7 @@ type Props = {
 }
 
 export const RemoteBigViewer: FC<Props> = ({ participant }) => {
+  const [forceHigh, setForceHigh] = useState(false)
   const camStream = usePeerRemoteStreamActive(participant.id!, BlueseaSenders.video.name)
   const micStream = usePeerRemoteStreamActive(participant.id!, BlueseaSenders.audio.name)
   const screenStream = usePeerRemoteStreamActive(participant.id!, BlueseaSenders.screen_video.name)
@@ -20,22 +21,23 @@ export const RemoteBigViewer: FC<Props> = ({ participant }) => {
 
   return (
     <>
-      <div className={classNames('w-full h-full', screenStream ? 'block' : 'hidden')}>
+      <div className="w-full h-full">
+        {screenStream && (
+          <Switch
+            className="absolute top-2 left-2 z-10"
+            checkedChildren="High"
+            unCheckedChildren="Auto"
+            checked={forceHigh}
+            onChange={setForceHigh}
+          />
+        )}
         <BigViewer
           participant={participant}
-          stream={screenStream as Stream}
-          priority={BlueseaStreamPriority.BigVideo}
-          isScreenShare
-          isTalking={isTalking}
-        />
-      </div>
-      <div className={classNames('w-full h-full', !screenStream ? 'block' : 'hidden')}>
-        <BigViewer
-          participant={participant}
-          stream={camStream as Stream}
+          stream={screenStream ? screenStream : (camStream as Stream)}
           micStream={micStream}
-          priority={BlueseaStreamPriority.BigVideo}
+          priority={screenStream ? BlueseaStreamPriority.ScreenShare : BlueseaStreamPriority.BigVideo}
           isTalking={isTalking}
+          forceHighQuality={screenStream && forceHigh}
         />
       </div>
     </>
