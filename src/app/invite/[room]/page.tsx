49acdc18ba6f39a2ac3ff56c8f81562c7af99e79
angtuaan <tuanna.mailbox@gmail.com>
2024-12-06 02:14:02 +0700

    fix: invite link

--- src/app/invite/[room]/page.tsx
+++ src/app/invite/[room]/page.tsx
@@ -1,7 +1,5 @@
 import { Invite } from '@/containers'
-import { cookies } from 'next/headers'
 
 export default async function InviteScreen() {
-  const username = (await cookies()).get('username')
-  return <Invite username={username} />
+  return <Invite />
 }
