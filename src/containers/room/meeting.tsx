adbf66f4fdb156f3c232c51225fc71c96634bde5
ivgnod <dongvt@ubox.vn>
2024-12-09 17:11:05 +0700

    update reset room user

--- src/containers/room/meeting.tsx
+++ src/containers/room/meeting.tsx
@@ -88,6 +88,13 @@ export const Meeting: React.FC<Props> = ({ host }) => {
       ],
       roomCode: (params?.room ?? '') as string,
     })
+
+    return () => {
+      addListUserToRoom({
+        users: [],
+        roomCode: (params?.room ?? '') as string,
+      })
+    }
   }, [addListUserToRoom, params?.room, remotePeers, user])
 
   return (
